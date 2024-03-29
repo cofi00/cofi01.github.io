451877fcdf6109f5838852605f0fec996aeba964
cofi00 <37311077+cofi00@users.noreply.github.com>
2024-03-29 11:43:00 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,14 +2,23 @@ from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
+
+received_data = {}
+
 @app.route('/api', methods=['POST'])
 def process_data():
+    global received_data
     # Pretpostavljamo da su podaci u JSON formatu
     data = request.json
+    received_data.update(data)  
+    return jsonify({"status": "Podaci primljeni"}), 200
 
-    # Vraćamo HTML stranicu sa popunjenim podacima
-    return render_template('index.html', ime=data['ime'], prezime=data['prezime'], email=data['email'], datum=data['datum'], broj=data['broj'])
+@app.route('/view-data', methods=['GET'])
+def view_data():
+   
+    return render_template('index.html', **received_data)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
+
 
